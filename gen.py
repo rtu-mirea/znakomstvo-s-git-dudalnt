b2d3617d8ee97c561f76925552d80d76cdd58d78
Alexandr <47754589+dudalnt@users.noreply.github.com>
2019-09-09 17:52:38 +0300

    Пример ПР
    * generator2
    
    * generator3

--- gen.py
+++ gen.py
@@ -1,6 +1,6 @@
 from datetime import timedelta,datetime,date
 
-def gen(startDate, endDate, step=timedelta(months=1), inclusive=True):
+def gen(startDate, endDate, step=timedelta(), inclusive=True):
     sdate = datetime.strptime(startDate, '%d.%m.%Y')
     edate = datetime.strptime(endDate, '%d.%m.%Y')
     if step.days > 0:
@@ -13,6 +13,6 @@ def gen(startDate, endDate, step=timedelta(months=1), inclusive=True):
             sdate = sdate + step
     if inclusive and sdate == edate:
         yield sdate
-fy = gen('30.04.1998', '11.05.2019', step=timedelta(months=1), inclusive=True)
+fy = gen('30.04.1998', '11.05.2019', step=timedelta(), inclusive=True)
 print(next(fy))
 
