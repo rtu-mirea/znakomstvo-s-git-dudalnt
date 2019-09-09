3131727997b61f59c495104d2b9ced7f0f71869b
Duda <dudakov.a.i.98@gmail.com>
2019-09-09 17:24:11 +0300

    generator

---
+++ gen.py
@@ -0,0 +1,18 @@
+from datetime import timedelta,datetime,date
+
+def gen(startDate, endDate, step=timedelta(months=1), inclusive=True):
+    sdate = datetime.strptime(startDate, '%d.%m.%Y')
+    edate = datetime.strptime(endDate, '%d.%m.%Y')
+    if step.days > 0:
+        while sdate < edate:
+            yield sdate
+            sdate = sdate + step
+    elif step.days < 0:
+        while sdate > edate:
+            yield sdate
+            sdate = sdate + step
+    if inclusive and sdate == edate:
+        yield sdate
+fy = gen('30.04.1998', '11.05.2019', step=timedelta(months=1), inclusive=True)
+print(next(fy))
+
